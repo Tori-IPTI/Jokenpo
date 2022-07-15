d7a7c874cec922a0e3d5bceec4cd074a2b7abe8a
Tori-IPTI <tori.ti@ipti.org.br>
2022-07-15 12:59:47 -0300

    placar teste  feito

--- test/pages/placar_page_test.dart
+++ test/pages/placar_page_test.dart
@@ -10,14 +10,14 @@ class MockJogo extends Mock implements Jogo {}
 void main() {
   testWidgets("When RegrasPage builds then show elements",
       (WidgetTester tester) async {
+    //Arrange
     final jogoMock = MockJogo();
-
-    when(() => jogoMock.partidas).thenReturn(0);
+    const qtdPartidas = 10;
+    when(() => jogoMock.partidas).thenReturn(qtdPartidas);
     when(() => jogoMock.empates).thenReturn(0);
     when(() => jogoMock.vitorias).thenReturn(0);
     when(() => jogoMock.derrotas).thenReturn(0);
 
-    //Arrange
     final page = Provider<Jogo>(
       create: (_) => jogoMock,
       child: const MaterialApp(
@@ -30,12 +30,66 @@ void main() {
     //Assert
 
     expect(
-      find.textContaining(
+      find.text(
         //textContaining procura o texto parcialmente
-        "As nossas partidas jogadas",
+        "As nossas partidas jogadas: $qtdPartidas",
       ),
       findsOneWidget,
     );
     expect(find.byType(ElevatedButton), findsNWidgets(3));
+  });
+  testWidgets("When RegrasPage show the values then verify",
+      (WidgetTester tester) async {
+    //Arrange
+    final jogoMock = MockJogo();
+    const qtdPartidas = 0;
+    const qtdEmpates = 0;
+    const qtdVitorias = 0;
+    const qtdDerrotas = 0;
+
+    when(() => jogoMock.partidas).thenReturn(qtdPartidas);
+    when(() => jogoMock.empates).thenReturn(qtdEmpates);
+    when(() => jogoMock.vitorias).thenReturn(qtdVitorias);
+    when(() => jogoMock.derrotas).thenReturn(qtdDerrotas);
+    //Act
+
+    final page = Provider<Jogo>(
+      create: (_) => jogoMock,
+      child: const MaterialApp(
+        home: PlacarPage(),
+      ),
+    );
+
+    await tester.pumpWidget(page);
+    //Assert
+
+    expect(
+      find.text(
+        //textContaining procura o texto parcialmente
+        "As nossas partidas jogadas: $qtdPartidas",
+      ),
+      findsOneWidget,
+    );
+    expect(
+      find.text(
+        //textContaining procura o texto parcialmente
+        "Suas vitórias: $qtdVitorias",
+      ),
+      findsOneWidget,
+    );
+    expect(
+      find.text(
+        //textContaining procura o texto parcialmente
+        "Suas derrotas: $qtdDerrotas",
+      ),
+      findsOneWidget,
+    );
+    expect(
+      find.text(
+        //textContaining procura o texto parcialmente
+        "Seus empates: $qtdEmpates",
+      ),
+      findsOneWidget,
+    );
   });
 }
